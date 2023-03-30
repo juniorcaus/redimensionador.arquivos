77ba284ea8d9ef4a4f36f105805fd6d7f6fb8d1d
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-03-30 18:55:20 -0300

    Criando novas pastas para redimencionamento

--- didaticos.redimencionador/Program.cs
+++ didaticos.redimencionador/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 
 namespace didaticos.redimencionador
@@ -20,14 +21,40 @@ namespace didaticos.redimencionador
 
         static void Redimensionar()
         {
-            for(int i = 0; i < 100; i++)
+            #region "Diretorios" 
+            string diretorio_entrada = "Arquivos_Entrada";
+            string diretorio_redimencionados = "Arquivos_Redimencionados";
+            string diretorio_finalizados = "Arquivos_Finalizados";
+
+
+            if (!Directory.Exists(diretorio_entrada)) ;
             {
-                while(true) {
+                Directory.CreateDirectory(diretorio_entrada);
+            }
+
+            if (!Directory.Exists(diretorio_redimencionados)) ;
+            {
+                Directory.CreateDirectory(diretorio_redimencionados);
+            }
+
+            if (!Directory.Exists(diretorio_finalizados)) ;
+            {
+                Directory.CreateDirectory(diretorio_finalizados);
+            }
+
+            #endregion 
+
+            while (true) 
+                {
+
+                //Meu programa vai olhar para pasta de entrada
+                // se tiver arquivo, ele irá dimencionar
+
+                Thread.Sleep(new TimeSpan(0, 0, 2));
 
                 }
 
-                Thread.Sleep(new TimeSpan(0, 0, 2));
-            }
+            
         }
     }
 }
