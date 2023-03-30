4c254ecce7e2bdbf50b734915d8268c53834aab2
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-03-29 21:13:20 -0300

    Adicionar arquivos de projeto.

---
+++ didaticos.redimencionador/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace didaticos.redimencionador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando projeto de redimencionador ");
+
+            Thread thread = new Thread(Redimensionar);
+            thread.Start();
+
+            
+
+                
+            Console.Read();
+        }
+
+        static void Redimensionar()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                while(true) {
+
+                }
+
+                Thread.Sleep(new TimeSpan(0, 0, 2));
+            }
+        }
+    }
+}
